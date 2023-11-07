ef49b077a7020a249aeda22fd6e8e01370984f7e
Samzie1 <samzielutalo@gmail.com>
2023-11-07 13:58:11 +0000

    first function

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+<>
+      <Header />
+    
+</>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <img src="./public/logo512.png" alt="logo"/>
+      <h1>React Flashcards</h1>
+      <p>🧠 Expand Your React Knowledge, One Flashcard at a Time!🐌</p>
+    </header>
+  );
+  }
+
+
+
+
+
+
+
+
+
+
+
+export default App;
