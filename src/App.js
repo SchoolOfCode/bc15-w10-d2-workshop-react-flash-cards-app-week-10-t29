680dbe21e6e6ff11a5ba96998346311a82d69533
Wing-Kit Leung <wing_kitleung@hotmail.com>
2023-11-07 14:08:14 +0000

    form function/component added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import logo from './logo.svg';
-
+import logo from "./logo.svg";
 
 function App() {
   return (
-<>
+    <>
       <Header />
-    
-</>
+      <Form />
+    </>
   );
 }
 
 function Header() {
   return (
     <header>
-      <img src="logo512.png" alt="logo"/>
+      <img src="logo192.png" alt="logo" width="50px" />
       <h1>React Flashcards</h1>
       <p>🧠 Expand Your React Knowledge, One Flashcard at a Time!🐌</p>
     </header>
   );
-  }
+}
 
-
-
-
-
-
-
-
-
-
+function Form() {
+  return (
+    <form>
+      <label for="question">Question: </label>
+      <input type="text" />
+      <label for="answer">Answer: </label>
+      <input type="text" />
+      <button>Add</button>
+    </form>
+  );
+}
 
 export default App;
