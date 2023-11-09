2ee546c005ec01132d8a3271e0ac62dc8b76b7b5
Wing-Kit Leung <wing_kitleung@hotmail.com>
2023-11-09 13:40:02 +0000

    trying edit flashcards

--- src/App.js
+++ src/App.js
@@ -161,7 +161,10 @@ function Flashcards({ flashcards, removeCard }) {
 
 function Flashcard({ flashcard, removeCard }) {
   const [change, setChange] = useState(true);
+  const [edit, setEdit] = useState(true);
   const [boxColor, setBoxColor] = useState("#323949");
+  const [editQuestion, setEditQuestion] = useState(flashcard.question);
+  const [editAnswer, setEditAnswer] = useState(flashcard.answer);
 
   function changeCard() {
     setChange((prevChange) => (prevChange ? false : true));
@@ -170,24 +173,77 @@ function Flashcard({ flashcard, removeCard }) {
     );
   }
 
+  function editCardContainer() {
+    setEdit((prevEdit) => (prevEdit ? false : true));
+  }
+
+  function handleEditQuestion(event) {
+    setEditQuestion(event.target.value);
+  }
+
+  function handleEditAnswer(event) {
+    setEditAnswer(event.target.value);
+  }
+
+  function clickEditButton(event) {
+    event.preventDefault();
+    const newFlashcard = {
+      id: flashcard.id,
+      question: editQuestion,
+      answer: editAnswer,
+    };
+    // addCard(newFlashcard);
+    // setQuestion("");
+    // setAnswer("");
+    editCard();
+  }
+
+  function editCard() {}
+
   return (
-    <div className="information" style={{ backgroundColor: boxColor }}>
-      <button
-        className="deleteButton"
-        style={{ backgroundColor: boxColor }}
-        onClick={() => removeCard(flashcard.id)}
-      >
-        ❌
-      </button>
-      <div className="flashcard" onClick={changeCard}>
-        {change ? <p className="emoji">🤔</p> : <p className="emoji">🤯</p>}
-        {change ? (
-          <p className="text">{flashcard.question}</p>
-        ) : (
-          <p className="text">{flashcard.answer}</p>
-        )}
-      </div>
-    </div>
+    <>
+      {" "}
+      {edit ? (
+        <div className="information" style={{ backgroundColor: boxColor }}>
+          <button
+            className="editButton"
+            style={{ backgroundColor: boxColor }}
+            onClick={() => editCardContainer()}
+          >
+            Edit
+          </button>
+          <button
+            className="deleteButton"
+            style={{ backgroundColor: boxColor }}
+            onClick={() => removeCard(flashcard.id)}
+          >
+            ❌
+          </button>
+          <div className="flashcard" onClick={changeCard}>
+            {change ? <p className="emoji">🤔</p> : <p className="emoji">🤯</p>}
+            {change ? (
+              <p className="text">{flashcard.question}</p>
+            ) : (
+              <p className="text">{flashcard.answer}</p>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div className="information" style={{ backgroundColor: "#323949" }}>
+          <form className="form" onSubmit={clickEditButton}>
+            <label for="question">Question: </label>
+            <input
+              type="text"
+              value={editQuestion}
+              onChange={handleEditQuestion}
+            />
+            <label for="answer">Answer: </label>
+            <input type="text" value={editAnswer} onChange={handleEditAnswer} />
+            <button type="submit">Add</button>
+          </form>
+        </div>
+      )}
+    </>
   );
 }
 
